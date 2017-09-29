c56080171dca715a22dc98e22c298e82bba407f5
Xavier Stevens <xstevens@users.noreply.github.com>
2017-09-28 17:04:02 -0700

    Changing write command to read secret value from stdin

--- cmd_write.go
+++ cmd_write.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"io/ioutil"
+	"os"
+	"strings"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ssm"
-
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 type writeCommand struct {
 	Name      string
-	Value     string
 	Overwrite bool
 }
 
@@ -17,7 +19,6 @@ func configureWriteCommand(app *kingpin.Application) {
 	wc := &writeCommand{}
 	write := app.Command("write", "Write secret to parameter store").Action(wc.runWrite)
 	write.Arg("name", "Secret name").StringVar(&wc.Name)
-	write.Arg("value", "Secret value").StringVar(&wc.Value)
 	write.Flag("overwrite", "Overwrite the existing secret").Default("false").BoolVar(&wc.Overwrite)
 }
 
@@ -29,12 +30,19 @@ func (wc *writeCommand) runWrite(ctx *kingpin.ParseContext) error {
 	}
 	ssmClient := ssm.New(sess, config)
 
+	// read secret value from stdin and convert to a string
+	data, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		return err
+	}
+	val := strings.TrimSpace(string(data))
+
 	// write the secret to the parameter store
 	ppInput := &ssm.PutParameterInput{
 		KeyId:     kmsAlias,
 		Name:      aws.String(wc.Name),
 		Type:      aws.String(ssm.ParameterTypeSecureString),
-		Value:     aws.String(wc.Value),
+		Value:     aws.String(val),
 		Overwrite: aws.Bool(wc.Overwrite),
 	}
 	_, err = ssmClient.PutParameter(ppInput)
